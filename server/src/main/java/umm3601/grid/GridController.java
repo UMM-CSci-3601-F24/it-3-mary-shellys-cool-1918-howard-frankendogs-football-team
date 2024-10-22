bb0170e84f90d4701c6529609857b739ba5170e0
Josie Barber <barbe497@umn.edu>
2024-10-22 15:38:02 -0500

    Add GridContoller.java and GridControllerSpec.java

---
+++ server/src/main/java/umm3601/grid/GridController.java
@@ -0,0 +1,24 @@
+package umm3601.grid;
+
+import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.websocket.WsContext;
+import umm3601.Controller;
+
+public class GridController implements Controller{
+
+
+
+
+
+    @Override
+    public void addRoutes(Javalin server) {
+        // TODO Auto-generated method stub
+
+        throw new UnsupportedOperationException("Unimplemented method 'addRoutes'");
+    }
+
+}
---
+++ server/src/test/java/umm3601/grid/GridControllerSpec.java
@@ -0,0 +1,5 @@
+package umm3601.grid;
+
+public class GridControllerSpec {
+
+}
