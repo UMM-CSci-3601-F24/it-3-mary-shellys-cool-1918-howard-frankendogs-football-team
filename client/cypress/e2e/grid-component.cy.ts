cc86d291bafd6560fb357d632c3fe1efc8e20ae6
Owen Beyer <beyer382@morris.umn.edu>
2024-10-23 15:27:40 -0500

    Added 1 e2e text
    yay!

--- client/cypress/e2e/grid-component.cy.ts
+++ client/cypress/e2e/grid-component.cy.ts
@@ -1,12 +1,27 @@
-
 describe('Grid Component', () => {
     beforeEach(() => {
-      cy.visit('/grid');
+      cy.visit('localhost:4200/grid');
     });
 
     it('should render the grid with default size', () => {
       cy.get('app-grid-component').within(() => {
         cy.get('mat-grid-tile').should('have.length', 100);
+      });
+    });
+  });
+
+  describe('Grid Component', () => {
+    beforeEach(() => {
+      cy.visit('localhost:4200/grid');
+    });
+
+    it('should render the grid with custom size', () => {
+
+      cy.get('#mat-input-0').type('{backspace}{backspace}11')
+
+      cy.get('app-grid-component').within(() => {
+        cy.get('mat-grid-tile').should('have.length', 100);
+
       });
     });
   });
