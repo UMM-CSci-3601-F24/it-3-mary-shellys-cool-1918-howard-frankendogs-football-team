209820c847f903d37ffb80a130663f584470b2cf
Josie Barber <barbe497@umn.edu>
2024-10-24 16:09:18 -0500

    Comment out part of the getGrids methods

--- server/src/main/java/umm3601/grid/GridController.java
+++ server/src/main/java/umm3601/grid/GridController.java
@@ -72,33 +72,36 @@ public class GridController implements Controller {
   //   }
   // }
 
-  // public void getGrids(Context ctx) {
-  //   List<Grid> gridsList = gridCollection.find().into(new ArrayList<>());
-  //   Grid[] gridsArray = gridsList.toArray(new Grid[0]);
-  //   ctx.json(gridsArray);  // public void getGrid(Context ctx) {
-  //   String id = ctx.pathParam("id");
-  //   Grid grid;
+  public void getGrids(Context ctx) {
+    List<Grid> gridsList = gridCollection.find().into(new ArrayList<>());
+    Grid[] gridsArray = gridsList.toArray(new Grid[0]);
+    // String id = ctx.pathParam("id");
+    // Grid grid;
 
-  //   try {
-  //     grid = gridCollection.find(eq("_id", new ObjectId(id))).first();
-  //   } catch (IllegalArgumentException e) {
-  //     throw new BadRequestResponse("The requested grid id wasn't a legal Mongo Object ID");
-  //   }
-  //   if (grid == null) {
-  //     throw new NotFoundResponse("The requested grid was not found");
-  //   } else {
-  //     ctx.json(grid);
-  //     ctx.status(HttpStatus.OK);
-  //   }
-  // }
-  //   ctx.status(HttpStatus.OK);
-  // }
+    // try {
+    //   grid = gridCollection.find(eq("_id", new ObjectId(id))).first();
+    // } catch (IllegalArgumentException e) {
+    //   throw new BadRequestResponse("The requested grid id wasn't a legal Mongo Object ID");
+    // }
+    // if (grid == null) {
+    //   throw new NotFoundResponse("The requested grid was not found");
+    // } else {
+    //   ctx.json(grid);
+    //   ctx.status(HttpStatus.OK);
+    // }
+
+    ctx.json(gridsArray);
+
+    // ctx.json(gridsList);
+
+  }
+
 
   @Override
   public void addRoutes(Javalin server) {
     // server.get(API_GRID_BY_ID, this::getGrid);
     server.post(API_SAVE_GRID, this::saveGrid);
-    // server.get(API_GET_GRIDS, this::getGrids);
+    server.get(API_GET_GRIDS, this::getGrids);
   }
 }
 
