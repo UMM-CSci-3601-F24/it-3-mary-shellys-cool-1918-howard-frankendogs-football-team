0ac9f0afdb3b4c0573b143044e3f9a2fa5f24269
El Anderson <and10393@umn.edu>
2024-11-03 20:56:51 -0600

    added seeding for coustom db
    need to make it seed and drop when e2e is ran still

--- client/cypress/e2e/grid-component.cy.ts
+++ client/cypress/e2e/grid-component.cy.ts
@@ -1,17 +1,32 @@
-import { GridPage } from "cypress/support/grid.po";
+import { GridPage } from 'cypress/support/grid.po';
 
 describe('Grid Component', () => {
     const page = new GridPage();
 
     beforeEach(() => {
       page.navigateTo();
+      cy.task('seed:database');
       // cy.visit('/grid');
     //   cy.visit('localhost:4200/grid');
     });
 
-    before(() => {
-      cy.task('seed:database');
-    })
+      it('should open saved grid to server', () => {
+        cy.get(':nth-child(3) > button').click();
+        cy.get(':nth-child(3) > button').click(); // the saved grid doesnt show up untill the second click
+        cy.url().should('include', '67282a76702f8c628808e130');
+        cy.get('#mat-input-218').should('have.css', 'background-color').and('eq', 'rgb(0, 0, 0)');
+      });
+
+      // it('should save a grid to server', () => {
+      //   cy.get('[data-test=saveGridButton]').click()
+      //   cy.intercept('api/grid').as('saveGrid');
+
+      //   cy.url({timeout: 300}).should('include', '_id');
+      //ideally would also have some sort of visual indication it was saved
+      // like implementing the visual of previously saved grids
+
+      //there should be something in the test here that looks at the actual server and checks that a new grid is there
+      // });
 
     it('should render the grid with default size', () => {
       cy.get('app-grid-component').within(() => {
@@ -19,17 +34,7 @@ describe('Grid Component', () => {
       });
     });
 
-    it('should save a grid to server', () => {
-      cy.get('[data-test=saveGridButton]').click()
-        // cy.intercept('api/grid').as('saveGrid');
-      page.saveGrid();
-        // cy.wait('@saveGrid');
-        // cy.url({timeout: 300000}).should('match', /\/grid$/); ------------ This is a current
-      //ideally would also have some sort of visual indication it was saved
-      // like implementing the visual of previously saved grids
 
-      //there should be something in the test here that looks at the actual server and checks that a new grid is there
-    })
 
   it('should render the grid with custom size', () => {
     cy.get('#mat-input-0').type('{backspace}{backspace}11')
