037a5964956dde8c0d3ece42d2dfaf8f02bd2d89
0beyer <beyer382@morris.umn.edu>
2024-11-11 22:13:31 -0600

    re formated stuff

--- client/cypress/e2e/grid-component.cy.ts
+++ client/cypress/e2e/grid-component.cy.ts
@@ -3,29 +3,28 @@ import { GridPage } from 'cypress/support/grid.po';
 describe('Grid Component', () => {
     const page = new GridPage();
 
-    beforeEach(() => {
-      page.navigateTo();
-      cy.task('seed:database');
+  beforeEach(() => {
+    page.navigateTo();
+    cy.task('seed:database');
+  });
+
+    it('should open saved grid to server', () => {
+      cy.get(':nth-child(3) > button').click();
+      cy.get(':nth-child(3) > button').click();
+      cy.intercept('api/grid').as('saveGrid');
+      cy.url().should('match', /\/grid\/67282673702f8c628808e12e$/);
+      cy.get('#mat-input-233').should('have.css', 'background-color').and('eq', 'rgb(0, 0, 0)');
     });
 
-      it('should open saved grid to server', () => {
-        cy.get(':nth-child(3) > button').click();
-        cy.get(':nth-child(3) > button').click();
-        cy.intercept('api/grid').as('saveGrid');
-        cy.url().should('match', /\/grid\/67282673702f8c628808e12e$/);
-        cy.get('#mat-input-233').should('have.css', 'background-color').and('eq', 'rgb(0, 0, 0)');
-      });
-
-      it('should save a grid to server', () => {
-        page.saveGrid();
-        cy.intercept('api/grid').as('saveGrid');
-
-        cy.url({timeout: 30000}).should('match', /\/grid$/);
+    it('should save a grid to server', () => {
+      page.saveGrid();
+      cy.intercept('api/grid').as('saveGrid');
+      cy.url({timeout: 30000}).should('match', /\/grid$/);
       //ideally would also have some sort of visual indication it was saved
       // like implementing the visual of previously saved grids
 
       //there should be something in the test here that looks at the actual server and checks that a new grid is there
-      });
+    });
 
     it('should render the grid with default size', () => {
       cy.get('app-grid-component').within(() => {
@@ -33,18 +32,16 @@ describe('Grid Component', () => {
       });
     });
 
-
-
-  it('should render the grid with custom size', () => {
-    cy.get('#mat-input-0').type('{backspace}{backspace}11')
-    cy.get('app-grid-component').within(() => {
-      cy.get('mat-grid-tile').should('have.length', 110);
+    it('should render the grid with custom size', () => {
+      cy.get('#mat-input-0').type('{backspace}{backspace}11')
+      cy.get('app-grid-component').within(() => {
+        cy.get('mat-grid-tile').should('have.length', 110);
+      });
     });
-  });
 
-  it('should black-out the cell with all edges bolded', () => {
-    cy.get('#mat-input-3').type('{ctrl}{click}');
-    cy.get('#mat-input-3').should('have.css', 'background-color').and('eq', 'rgb(0, 0, 0)');
+    it('should black-out the cell with all edges bolded', () => {
+      cy.get('#mat-input-3').type('{ctrl}{click}');
+      cy.get('#mat-input-3').should('have.css', 'background-color').and('eq', 'rgb(0, 0, 0)');
   });
 });
 
