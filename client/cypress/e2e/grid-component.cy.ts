91fa7639e235a2ba0701602a736886e1538270b4
0beyer <beyer382@morris.umn.edu>
2024-11-17 12:19:19 -0600

    forgot to uncomment a test :p

--- client/cypress/e2e/grid-component.cy.ts
+++ client/cypress/e2e/grid-component.cy.ts
@@ -8,25 +8,25 @@ describe('Grid Component', () => {
     cy.task('seed:database');
   });
 
-  // it('should open saved grid to server', () => {
-  //   cy.get(':nth-child(3) > button').click();
-  //   cy.get(':nth-child(3) > button').click();
-  //   cy.intercept('api/grid').as('saveGrid');
-  //   cy.url().should('match', /\/grid\/67282673702f8c628808e12e$/);
-  //   cy.get('#mat-input-233')
-  //     .should('have.css', 'background-color')
-  //     .and('eq', 'rgb(0, 0, 0)');
-  // });
+  it('should open saved grid to server', () => {
+    cy.get(':nth-child(3) > button').click();
+    cy.get(':nth-child(3) > button').click();
+    cy.intercept('api/grid').as('saveGrid');
+    cy.url().should('match', /\/grid\/67282673702f8c628808e12e$/);
+    cy.get('#mat-input-233')
+      .should('have.css', 'background-color')
+      .and('eq', 'rgb(0, 0, 0)');
+  });
 
-  // it('should save a grid to server', () => {
-  //   page.saveGrid();
-  //   cy.intercept('api/grid').as('saveGrid');
-  //   cy.url({ timeout: 30000 }).should('match', /\/grid$/);
-  //   //ideally would also have some sort of visual indication it was saved
-  //   // like implementing the visual of previously saved grids
+  it('should save a grid to server', () => {
+    page.saveGrid();
+    cy.intercept('api/grid').as('saveGrid');
+    cy.url({ timeout: 30000 }).should('match', /\/grid$/);
+    //ideally would also have some sort of visual indication it was saved
+    // like implementing the visual of previously saved grids
 
-  //   //there should be something in the test here that looks at the actual server and checks that a new grid is there
-  // }); this test is freaked on my box, ill get it to work on a box with mongo
+    //there should be something in the test here that looks at the actual server and checks that a new grid is there
+  });
 
   it('should render the grid with default size', () => {
     cy.get('app-grid-component').within(() => {
