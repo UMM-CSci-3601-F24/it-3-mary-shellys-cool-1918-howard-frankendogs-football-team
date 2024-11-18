adf852df458945956335ecf2f70647b664345809
Owen Beyer <beyer382@morris.umn.edu>
2024-11-18 15:25:01 -0600

    fixed my e2e test
    :p

--- client/cypress/e2e/grid-component.cy.ts
+++ client/cypress/e2e/grid-component.cy.ts
@@ -8,25 +8,23 @@ describe('Grid Component', () => {
     cy.task('seed:database');
   });
 
-  // it('should open saved grid to server', () => {
-  //   cy.get(':nth-child(3) > button').click();
-  //   cy.get(':nth-child(3) > button').click();
-  //   cy.intercept('api/grid').as('saveGrid');
-  //   cy.url().should('match', /\/grid\/67282673702f8c628808e12e$/);
-  //   cy.get('#mat-input-233')
-  //     .should('have.css', 'background-color')
-  //     .and('eq', 'rgb(0, 0, 0)');
-  // });
+  it('should open saved grid to server', () => {
+    cy.get(':nth-child(1) > :nth-child(2) > :nth-child(2) > button').click();
+    cy.get(':nth-child(1) > :nth-child(2) > :nth-child(2) > button').click();
+    cy.intercept('api/grid').as('saveGrids');
+    cy.url().should('contain', '/seededGrids/grid/673ba3a31e6a570b74f9a310');
+  });
 
-  // it('should save a grid to server', () => {
-  //   page.saveGrid();
-  //   cy.intercept('api/grid').as('saveGrid');
-  //   cy.url({ timeout: 30000 }).should('match', /\/grid$/);
-    //ideally would also have some sort of visual indication it was saved
+  it('should save a grid to server', () => {
+    page.saveGrid();
+    cy.get(':nth-child(3) > button').click();
+    cy.get(':nth-child(3) > button').click();
+    cy.url().should('contain', '/seededGrids/grid');
+    // ideally would also have some sort of visual indication it was saved
     // like implementing the visual of previously saved grids
 
-    //there should be something in the test here that looks at the actual server and checks that a new grid is there
-  // });
+    // there should be something in the test here that looks at the actual server and checks that a new grid is there
+  });
 
   it('should render the grid with default size', () => {
     cy.get('app-grid-component').within(() => {
