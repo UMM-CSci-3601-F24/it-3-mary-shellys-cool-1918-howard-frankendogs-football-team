3af249e4c2ba18dbf2638d9e4c0d181060a77726
El Anderson <and10393@umn.edu>
2024-11-19 16:36:22 -0600

    adds buttons for word groups on anagram page, removes delete by word group button
    words and search history are still not coming back from server, button added does not work

--- client/src/app/anagram/word-group-profile.component.ts
+++ client/src/app/anagram/word-group-profile.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { WordService } from './word.service';
+import { RoomService } from '../room.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-word-group-profile',
@@ -8,5 +13,13 @@ import { Component } from '@angular/core';
   styleUrl: './word-group-profile.component.scss'
 })
 export class WordGroupProfileComponent {
+  private route = inject(ActivatedRoute);
+  private roomService = inject(RoomService)
+  private wordService = inject(WordService)
 
+  wordGroup = toSignal(
+    this.route.paramMap.pipe(
+      map((paramMap: ParamMap) => paramMap.get(`wordGroup`)),
+    )
+  )
 }
