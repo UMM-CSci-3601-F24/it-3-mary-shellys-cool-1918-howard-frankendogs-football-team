e663152a50909320418739591ac449e5cf831b32
El <and10393@umn.edu>
2024-12-04 22:38:29 -0600

    Fixes Filter by Word length
    the observable for length was not being included in the switch map

--- client/src/app/anagram/word-list.component.ts
+++ client/src/app/anagram/word-list.component.ts
@@ -76,14 +76,14 @@ export class WordListComponent {
 
   private contains$ = toObservable(this.contains);
   private group$ = toObservable(this.group);
+  private length$ = toObservable(this.length);
   private filterType$ = toObservable(this.filterType);
   private forceUpdate$ = toObservable(this.forceUpdate);
-  private length$ = toObservable(this.length);
 
   serverFilteredContext =
     toSignal(
-      combineLatest([this.contains$, this.group$, this.filterType$, this.forceUpdate$, this.length$]).pipe(
-        switchMap(([word, wordGroup, filterType]) =>
+      combineLatest([this.contains$, this.group$, this.filterType$, this.length$, this.forceUpdate$,]).pipe(
+        switchMap(([word, wordGroup, filterType, length]) =>
           this.wordService.getWords({
             word,
             wordGroup,
@@ -105,11 +105,10 @@ export class WordListComponent {
           return of<SearchContext>();
         }),
         tap(() => {
-
         })
       )
     );
-  
+
   filteredWords = computed(() => {
     // takes list of words returned by server
     // then sends them through the client side sortWords()
